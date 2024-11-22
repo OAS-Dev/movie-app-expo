2b65654cf5f2424758c072fec62f7b1355f54684
Oscar Ariel Saucedo <arielsaucedo.dev@gmail.com>
2024-11-22 16:06:10 -0300

    ✨ Mapper
    Mapper is created for personalized response regarding the movies to be shown in the app

--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,12 +1,13 @@
 import {movieApi} from '@/core/api/movie-api';
-import {MovieDBMovieResponse} from '@/infrastructure/interfaces/movieDb.response';
+import {MovieDBMovieResponse} from '@/infrastructure/interfaces/moviedb.response';
+import {MovieMapper} from '@/infrastructure/mappers/movie.mapper';
 
 export const nowPlayingAction = async () => {
   try {
     const {data} = await movieApi.get<MovieDBMovieResponse>('/now_playing');
-    console.log(JSON.stringify(data, null, 2));
+    const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
 
-    return [];
+    return movies;
   } catch (error) {
     console.log(error);
     throw 'Cannot fetch movies';
