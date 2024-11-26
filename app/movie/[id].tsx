7612514ae31ec45bfcf7bd846d5c73f6fb986ea8
Oscar Ariel Saucedo <arielsaucedo.dev@gmail.com>
2024-11-25 23:37:50 -0300

    ✨ Movie Screen
    MovieScreen is created and the header with the image is created

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,16 +1,31 @@
-import {View, Text} from 'react-native';
+import {View, Text, ActivityIndicator, ScrollView} from 'react-native';
 import {useLocalSearchParams} from 'expo-router';
-import {getMovieByIdAction} from '../../core/actions/movie/get-movie-by-id.action';
+
+import {useMovie} from '@/presentation/hooks/useMovie';
+import MovieHeader from '@/presentation/components/movie/MovieHeader';
 
 const MovieScreen = () => {
   const {id} = useLocalSearchParams();
 
-  getMovieByIdAction(+id);
+  const {movieQuery} = useMovie(+id);
+
+  if (movieQuery.isLoading || !movieQuery.data) {
+    return (
+      <View className='flex-1 items-center justify-center'>
+        <Text className='mb-4'>Espere por favor...</Text>
+        <ActivityIndicator size='large' color='#0000ff' />
+      </View>
+    );
+  }
 
   return (
-    <View>
-      <Text>MovieScreen</Text>
-    </View>
+    <ScrollView>
+      <MovieHeader
+        poster={movieQuery.data.poster}
+        originalTitle={movieQuery.data.originalTitle}
+        title={movieQuery.data.title}
+      />
+    </ScrollView>
   );
 };
 
